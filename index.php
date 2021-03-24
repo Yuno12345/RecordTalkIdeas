517a0250d330c8cf4ddc686f929bdca037df6bea
yunosuke <yuu0209ide@gmail.com>
2021-03-25 07:39:33 +0900

    update layout

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ if($_POST){
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>RecordTalkIdeas</title>
     <link rel="stylesheet" href="login_style.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
