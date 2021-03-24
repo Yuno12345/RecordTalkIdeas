36bc05e6f37e37ea1acbeac20352fb947eb73036
yunosuke <yuu0209ide@gmail.com>
2021-03-24 19:02:32 +0900

    update categoryid to categoryname

--- talkcontents/list.php
+++ talkcontents/list.php
@@ -14,7 +14,7 @@ login_check();
 $member_id=$_SESSION["user_id"];
 
 if(!empty($_GET["category"])){
-  $category_id=h($_GET["category"]);
+  $category_name=h($_GET["category"]);
 }
 else{
   header("Location:./index.php");
@@ -25,8 +25,8 @@ if(!empty($_POST["sort"])){
 }
 
 //アイディア数取得
-$getcount=$db->prepare('SELECT count(*) as cnt FROM ideas WHERE category_id=? AND member_id=?');
-$getcount->execute(array($category_id,$member_id));
+$getcount=$db->prepare('SELECT count(*) as cnt FROM ideas WHERE category_name=? AND member_id=?');
+$getcount->execute(array($category_name,$member_id));
 $count=$getcount->fetch();
 
 //ページの指定がなければ、1ページ目を表示
@@ -51,22 +51,22 @@ if($sort){
     //ORDER BY句はプリペアドメソッドを使えないので、switch文で条件分岐
     switch($sort){
         case 'self_evaluate':
-          $category_contents=$db->prepare("SELECT * FROM ideas WHERE category_id=? AND member_id=? ORDER BY self_evaluate DESC LIMIT $startdisp , 5");
-          $category_contents->execute(array($category_id,$member_id));
+          $category_contents=$db->prepare("SELECT * FROM ideas WHERE category_name=? AND member_id=? ORDER BY self_evaluate DESC LIMIT $startdisp , 5");
+          $category_contents->execute(array($category_name,$member_id));
         break;
 
         case 'modified':
-            $category_contents=$db->prepare("SELECT * FROM ideas WHERE category_id=? AND member_id=? ORDER BY modified DESC LIMIT $startdisp , 5");
-            $category_contents->execute(array($category_id,$member_id));
+            $category_contents=$db->prepare("SELECT * FROM ideas WHERE category_name=? AND member_id=? ORDER BY modified DESC LIMIT $startdisp , 5");
+            $category_contents->execute(array($category_name,$member_id));
         break;
 
         default:
-            $category_contents=$db->prepare("SELECT * FROM ideas WHERE category_id=? AND member_id=? ORDER BY created DESC LIMIT $startdisp , 5");
-            $category_contents->execute(array($category_id,$member_id));
+            $category_contents=$db->prepare("SELECT * FROM ideas WHERE category_name=? AND member_id=? ORDER BY created DESC LIMIT $startdisp , 5");
+            $category_contents->execute(array($category_name,$member_id));
     }
 }else{
-    $category_contents=$db->prepare("SELECT * FROM ideas WHERE category_id=? AND member_id=? ORDER BY created DESC LIMIT $startdisp , 5");
-    $category_contents->execute(array($category_id,$member_id));
+    $category_contents=$db->prepare("SELECT * FROM ideas WHERE category_name=? AND member_id=? ORDER BY created DESC LIMIT $startdisp , 5");
+    $category_contents->execute(array($category_name,$member_id));
 }
 
 $ideas=$category_contents->fetchAll();
@@ -92,7 +92,7 @@ $good_records=$getgood->fetchAll();
   <header>
       <div class="top-wrapper clearfix">
         <div class="create">
-          <a class="newcreate" href="create.php?category=<?php echo $category_id."&state=new"?>">新規作成</a>
+          <a class="newcreate" href="create.php?category=<?php echo $category_name."&state=new"?>">新規作成</a>
         </div>
         <div class="header-logo">
           <a href="./index.php"><img src="../RTI-rogo.png" width="400px" height="60px" /></a>
@@ -109,7 +109,7 @@ $good_records=$getgood->fetchAll();
 
     <div class="sort-wrapper"> 
         <div class="container">
-         <form action="./list.php?category=<?php echo $category_id?>" method="post">
+         <form action="./list.php?category=<?php echo $category_name?>" method="post">
             <select class="sort" id="sort" name="sort">
                 <option value="created" <?= $sort != 'created' ?: 'selected' ?>>作成順</option>
                 <option value="self_evaluate" <?= $sort != 'self_evaluate' ?: 'selected' ?>>評価順</option>
@@ -125,7 +125,7 @@ $good_records=$getgood->fetchAll();
       <dl>
         <?php foreach($ideas as $idea):?>
           <div class="idea-info">
-            <dt><a href="./view/index.php?category=<?php echo $category_id."&idea=".$idea["id"]?>"><?php echo $idea["title"]?></a>
+            <dt><a href="./view/index.php?category=<?php echo $category_name."&idea=".$idea["id"]?>"><?php echo $idea["title"]?></a>
               <div class="evaluation-button" data-idea_id="<?=$idea["id"]?>">
                 <span class="good fa fa-thumbs-up">
                   <?php foreach($good_records as $good_record):?>
@@ -138,8 +138,8 @@ $good_records=$getgood->fetchAll();
             </dt>
             <dd>自己評価：<?php echo $idea["self_evaluate"]?>/5</dd>
             <dd>作成日：<?php echo $idea["created"]?>
-              <a href="./create.php?category=<?=$category_id?>&id=<?=$idea["id"]?>&state=edit">編集</a>
-              <a href="./create.php?category=<?=$category_id?>&id=<?=$idea["id"]?>&state=delete" onclick="return delete_alert()">消去</a>
+              <a href="./create.php?category=<?=$category_name?>&id=<?=$idea["id"]?>&state=edit">編集</a>
+              <a href="./create.php?category=<?=$category_name?>&id=<?=$idea["id"]?>&state=delete" onclick="return delete_alert()">消去</a>
             </dd>
           </div>
         <?php endforeach?>
@@ -148,14 +148,14 @@ $good_records=$getgood->fetchAll();
 
     <div class="page">
       <?php if($page!=1):?>
-        <form method="post" name="form1" action="./list.php?category=<?=$category_id?>&page=<?=$page-1?>">
+        <form method="post" name="form1" action="./list.php?category=<?=$category_name?>&page=<?=$page-1?>">
           <input type="hidden" name="sort" value=<?=$sort?>>
           <a href="javascript:form1.submit()">前へ</a>
         </form>
       <?php endif?>
       
       <?php if($page!=$maxpage):?>
-        <form method="post" name="form2" action="./list.php?category=<?=$category_id?>&page=<?=$page+1?>">
+        <form method="post" name="form2" action="./list.php?category=<?=$category_name?>&page=<?=$page+1?>">
           <input type="hidden" name="sort" value=<?=$sort?>>
           <a href="javascript:form2.submit()">次へ</a>
         </form>
